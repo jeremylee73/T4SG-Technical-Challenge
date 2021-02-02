61fa1907f2375206ea81d59c7429443b5590809d
Jeremy Lee <jeremylee@Jeremys-MacBook-Pro.local>
2021-02-01 19:33:37 -0500

    Added basic index.html

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
