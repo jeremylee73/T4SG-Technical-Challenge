232542996f3f4ef58b7204f74fc1a37b91114378
Jeremy Lee <jeremylee@Jeremys-MacBook-Pro.local>
2021-02-01 19:47:36 -0500

    Added register.html and new route

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Registers user"""
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        return redirect("/")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
